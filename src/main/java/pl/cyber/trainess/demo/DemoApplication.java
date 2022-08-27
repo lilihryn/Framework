429785260d1f108110dd084c6df1919c3cd7074a
lilihryn <108538332+lilihryn@users.noreply.github.com>
2022-08-27 09:09:26 +0200

    Inicializacja SpringBoot App

---
+++ src/main/java/pl/cyber/trainess/demo/DemoApplication.java
@@ -0,0 +1,11 @@
+package pl.cyber.trainess.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication//adnotacja którem zadaniem jesturuchomienie aplikacji springowej
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class,args);//linii kodu które nie mogą być zmodefikowane
+    }
+}

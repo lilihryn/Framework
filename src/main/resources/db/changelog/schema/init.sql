95787f338f5ad385dcae6aeeb6598cd7291a8230
lilihryn <108538332+lilihryn@users.noreply.github.com>
2022-09-23 20:38:24 +0200

    praca z sql przez java

---
+++ src/main/resources/db/changelog/schema/init.sql
@@ -0,0 +1,19 @@
+create table bankomat(
+          id         varchar(40),--UUID: ea1b5aeb-ab59-41de-abfa-350bd01e6ece
+
+          name       varchar(255),
+          saldo      DECIMAL,
+          miasto     varchar(255),
+          ulica      varchar(255),
+          czy_aktywny   boolean,
+          PRIMARY KEY(id)
+          );
+
+       create table konto_bankowe(
+       id          varchar(255) PRIMARY KEY,
+       imie        varchar(255),
+       nazwisko    varchar(255),
+       saldo       DECIMAL,
+       data_ur     timestamp,
+       miasto      varchar(255)
+       );
